85cff19115e1e59eb7a4c41af4796222e8ef6f0a
Snehal Reddy Yelmati <snehalyelmati@gmail.com>
2023-05-09 19:43:08 +0530

    Implement POC using LLMs

---
+++ main.py
@@ -0,0 +1,126 @@
+from flask import Flask, jsonify, request
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM, BloomTokenizerFast, \
+    BloomForCausalLM
+
+# Creating a Flask app
+app = Flask(__name__)
+
+
+# os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"]="0.0"
+
+# To check the if the API is up
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    if request.method == 'GET':
+        data = "API is up!"
+        return jsonify({'status': data})
+
+
+@app.route('/llm_initialize', methods=['GET'])
+def llm_initialize():
+    if request.method == 'GET':
+        global tokenizer
+        global model
+        global device
+
+        # path = "bigscience/bloomz-560m"
+        # path = "bigscience/bloomz-1b7"
+        # path = "bigscience/bloomz-3b"
+        # path = "bigscience/bloomz-7b1"
+
+        # path = "declare-lab/flan-alpaca-large"
+        # path = "declare-lab/flan-gpt4all-xl"
+
+        # path = "lmsys/vicuna-13b-delta-v1.1"
+        # path = "AlekseyKorshuk/vicuna-7b"
+        # path = "TheBloke/stable-vicuna-13B-GPTQ"
+        # path = "TheBloke/stable-vicuna-13B-HF"
+        path = "TheBloke/stable-vicuna-13B.ggml.q4"
+
+        device = "mps"
+
+        print(f'Loading model started from path: {path}')
+
+        if "bloom" in path:
+            tokenizer = BloomTokenizerFast.from_pretrained(path)
+            model = BloomForCausalLM.from_pretrained(path, low_cpu_mem_usage=True)
+        elif "vicuna" in path:
+            tokenizer = AutoTokenizer.from_pretrained(path)
+            model = AutoModelForCausalLM.from_pretrained(path, low_cpu_mem_usage=True)
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(path)
+            model = AutoModelForSeq2SeqLM.from_pretrained(path, low_cpu_mem_usage=True)
+
+        model.to(device)
+        print(f'Loading model finished...')
+
+        return jsonify({'result': 'success'})
+
+
+# TODO: implement llm_save method functionality to handle other model types
+@app.route('/llm_save', methods=['POST'])
+def llm_save():
+    if request.method == 'POST':
+        print(f'Saving model started...')
+        save_path = "/Users/snehalyelmati/Documents/models/"
+        model_name = request.form["model_name"]
+        tokenizer = BloomTokenizerFast.from_pretrained(model_name)
+        model = BloomForCausalLM.from_pretrained(model_name)
+
+        print(f'Save path: {save_path + model_name}')
+        model.save_pretrained(save_path + model_name)
+        tokenizer.save_pretrained(save_path + model_name)
+        print(f'Saving model finished...')
+
+        return jsonify({'result': 'success'})
+
+
+def llm(input: str, is_full_prompt="False"):
+    print("LLM prediction started...")
+    # generator = pipeline("text2text-generation", model, tokenizer, device=device)
+    # print(f'Pipeline output: {generator(input, max_length=150, num_return_sequences=1)}')
+
+    if is_full_prompt == "True":
+        inputs = tokenizer(input, return_tensors="pt")["input_ids"].to(device)
+    else:
+        advice = input
+        # advice = "Invest in company XYZ, the returns would surely triple in a year."
+        inputs = tokenizer(
+            f'''Assume you are a financial adviser who flags fraudulent advices. Your
+task is to review the advice, delimited by <>, given by another
+financial advisor to their client.
+
+Question: Is the advice given by the financial adviser fraudulent?
+
+Format your output as a valid JSON object with the following keys,
+
+1. "Reasoning" - reasoning for the question above.
+2. "Final answer" - final answer whether the advice is fraudulent. “Yes” if the advice is fraudulent, “No” if it is not fraudulent.
+
+Do not include any additional information apart from the information that is requested above.
+
+Advice: {advice}> 
+Output:
+'''
+            , return_tensors="pt")["input_ids"].to(device)
+
+    # inputs = tokenizer('A cat in French is "', return_tensors="pt")["input_ids"].to(device)
+    outputs = model.generate(inputs, max_length=1000)
+    decoded_output = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(decoded_output)
+    print(f'LLM prediction finished!')
+    return decoded_output
+
+
+@app.route('/llm_predict', methods=['POST'])
+def llm_predict():
+    if request.method == 'POST':
+        data = request.form["utterance"]
+        is_full_prompt = request.form["is_full_prompt"]
+        result = llm(str(data), is_full_prompt)
+        return jsonify({'result': result})
+
+
+# driver function
+if __name__ == '__main__':
+    app.run(port=5999, debug=True)
